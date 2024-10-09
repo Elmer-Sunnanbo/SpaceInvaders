971e63985afc3b7992199c77ef2bd6017866533f
Arkhevist <elmer.d.j.sunnanbo@gmail.com>
2024-10-09 19:59:56 +0200

    Added base project
    Minor tweaks were necessary to make it even functional

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class Laser : Projectile
+{
+    private void Awake()
+    {
+        direction = Vector3.up;
+    }
+
+    void Update()
+    {
+        transform.position += speed * Time.deltaTime * direction;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        CheckCollision(collision);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject); //The boundary is the only object with a collider (not a trigger)
+    }
+
+    void CheckCollision(Collider2D collision)
+    {
+        Bunker bunker = collision.gameObject.GetComponent<Bunker>();
+
+        if(bunker == null) //Om det inte är en bunker vi träffat så ska skottet försvinna.
+        {
+            Destroy(gameObject);
+        }
+    }
+}
