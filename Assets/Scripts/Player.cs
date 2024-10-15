7e0f7f51bf58bb6c126391628b44ca5727407f40
Noakinho <noak.larsson@elev.ga.lbs.se>
2024-10-15 12:11:21 +0200

    Weapon Switch
    lagg till så man kan byta vapen genom att trycka 1, 2 eller 3

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,17 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(BoxCollider2D))]
 public class Player : MonoBehaviour
 {
     public Laser laserPrefab;
-    Laser laser;
-    float speed = 5f;
+    public Shotgun shotgunPrefab;
+    public Grenade grenadePrefab;
 
-    // Update is called once per frame
+    private Laser laser;
+    private Shotgun shotgun;
+    private Grenade grenade;
+
+    private float speed = 5f;
+
+    public enum WeaponType { Laser, Shotgun, Grenade }
+    private WeaponType currentWeapon = WeaponType.Laser;
+
     void Update()
+    {
+        HandleMovement();
+        HandleShooting();
+        HandleWeaponSwitching();
+    }
+
+    private void HandleMovement()
     {
         Vector3 position = transform.position;
 
@@ -29,16 +43,52 @@ public class Player : MonoBehaviour
         Vector3 rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
 
         position.x = Mathf.Clamp(position.x, leftEdge.x, rightEdge.x);
-
         transform.position = position;
-        
+    }
 
-        if (Input.GetKeyDown(KeyCode.Space) && laser == null)
+    private void HandleShooting()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            laser = Instantiate(laserPrefab, transform.position + new Vector3(0,2), Quaternion.identity);
+            switch (currentWeapon)
+            {
+                case WeaponType.Laser:
+                    if (laser == null)
+                    {
+                        laser = Instantiate(laserPrefab, transform.position + new Vector3(0, 2), Quaternion.identity);
+                    }
+                    break;
+                case WeaponType.Shotgun:
+                    if (shotgun == null)
+                    {
+                        shotgun = Instantiate(shotgunPrefab, transform.position + new Vector3(0, 2), Quaternion.identity);
+                    }
+                    break;
+                case WeaponType.Grenade:
+                    if (grenade == null)
+                    {
+                        grenade = Instantiate(grenadePrefab, transform.position + new Vector3(0, 2), Quaternion.identity);
+                    }
+                    break;
+            }
         }
     }
 
+    private void HandleWeaponSwitching()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentWeapon = WeaponType.Laser;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentWeapon = WeaponType.Shotgun;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            currentWeapon = WeaponType.Grenade;
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Missile") || collision.gameObject.layer == LayerMask.NameToLayer("Invader"))
